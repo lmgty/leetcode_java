ca9402bcb0b9b8864b21fdb2aeb07332c9aab299
lmgty <1063042553@qq.com>
2020-09-27 21:59:07 +0800

    finish q98

---
+++ src/main/java/ValidateBinarySearchTree98.java
@@ -0,0 +1,40 @@
+/**
+ * @author LiuYe
+ * @date 2020/9/24 10:04 下午
+ */
+public class ValidateBinarySearchTree98 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(15);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(20);
+
+        System.out.println(isValidBST(root));
+    }
+
+
+    public static boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    public static boolean helper(TreeNode root, Integer lower, Integer upper) {
+        if (root == null) {
+            return true;
+        }
+        if (lower != null && root.val <= lower) {
+            return false;
+        }
+        if (upper != null && root.val >= upper) {
+            return false;
+        }
+        if (!helper(root.left,lower,root.val)){
+            return false;
+        }
+        if (!helper(root.right,root.val, upper)){
+            return false;
+        }
+        return true;
+    }
+}
