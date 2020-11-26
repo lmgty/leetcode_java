fe5e24cc87b989ed9be4f734cd8fbc8509b3c617
YeLiu <1063042553@qq.com>
2020-11-26 17:03:32 +0800

    finish q222

---
+++ src/main/java/Q222.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author LiuYe
+ * @data 2020/11/24
+ */
+public class Q222 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        root.right.right.left = new TreeNode(7);
+        System.out.println(countNodes(root));
+    }
+
+    public static int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        return left + right + 1;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
